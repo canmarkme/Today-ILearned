5664a40ac2c8f4d75023a61f63bd46a82fb29e44
canmarkme <numnice@hanmail.net>
2021-09-20 03:53:05 +0900

    [new] 210920 TIL

---
+++ PS/BOJ/b10/b10.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+
+function solution(num1, num2) {
+  // Write your code
+  console.log();
+}
---
+++ PS/BOJ/b2884/b2884.js
@@ -0,0 +1,33 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  let input = line.split(" ");
+  let H = Number(input[0]);
+  let M = Number(input[1]);
+  solution(H, M);
+
+
+  function solution(H, M) {
+    if (M - 45 < 0) {
+      M = 60 + (M - 45);
+      H -= 1;
+
+      if (H === -1) {
+        H = 23;
+      }
+    } else {
+      M -= 45;
+    }
+
+    console.log(H + " " + M);
+  }
+}).on("close", function () {
+  process.exit();
+});
