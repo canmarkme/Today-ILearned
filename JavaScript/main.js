12230a45ec1663a3bd12515107c0c455b00916cf
canmarkme <numnice@hanmail.net>
2021-09-26 12:54:41 +0900

    [new] 210926 TIL

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,4 +1,2 @@
-const fs = require("fs");
-const input = fs.readFileSync("./input.txt", "utf8");
-
-console.log(input);
+var template = '<ul>\n\t<li><a herf="#">Home</a></li>\n</ul>';
+console.log(template);
