243e38e5ea851560ab31d9268a32736d2367fadc
canmarkme <numnice@hanmail.net>
2021-10-14 17:40:34 +0900

    [new] 211014 TIL

--- PS/Programmers/Level1/같은숫자는싫어/같은숫자는싫어.js
+++ PS/Programmers/Level1/같은숫자는싫어/같은숫자는싫어.js
@@ -1,5 +1,6 @@
 let input = [
-
+  [1, 1, 3, 3, 0, 1, 1],
+  [4, 4, 4, 3, 3]
 ];
 
 for (let i = 0; i < input.length; i++) {
@@ -7,9 +8,26 @@ for (let i = 0; i < input.length; i++) {
   console.log(solution(input[i]));
 }
 
-function solution(num) {
-  var answer = 0;
+
+// 강사님 방법 2) for문의 if를 filter로 쓸 수 있습니다. 
+function solutio  return arr.filter((v, i) => v != arr[i + 1]);
+}
+
+
+// 강사님 방법 1)
+// 인접 중복된 숫자만 제거하기 때문에 Set을 사용할 수 없습니다.
+function solution(arr) {
+  let answer = [];
   console.log();
+
+  // 1. 인접한 이전 숫자와 현재 숫자가 같은 숫자인가?
+  //    같으면 자신을 remove. 최종적 숫자가 살아남는다. push
+  for (let i = 0; i < arr.length - 1; i++) {  // 마지막 요소는 i+1이 없기 때문에 length-1을 합니다.
+    if (arr[i] != arr[i + 1]) {
+      answer.push(arr[i]);
+    }
+  }
+  answer.push(arr[arr.length - 1]);     // 마지막 요소는 충돌할 요소가 없으니 push 해줍니다.
 
   return answer;
 }
--- PS/Programmers/Level1/나누어떨어지는숫자배열/나누어떨어지는숫자배열.js
+++ PS/Programmers/Level1/나누어떨어지는숫자배열/나누어떨어지는숫자배열.js
@@ -1,15 +1,35 @@
 let input = [
-
+  [[5, 9, 7, 10], 5],
+  [[2, 36, 1, 3], 1],
+  [[3, 2, 6], 10]
 ];
 
 for (let i = 0; i < input.length; i++) {
   process.stdout.write(`#${i + 1} `);
-  console.log(solution(input[i]));
+  console.log(solution(input[i][0], input[i][1]));
 }
 
-function solution(num) {
-  var answer = 0;
-  console.log();
+// 강사님 방법 1) for문을 안쓰는 방법으로 filter 사용
+function solution(arr, divisor) {
 
-  return answer;
+  // 1. arr 초기화
+  let answer = arr.filter(n => n & divisor == 0);
+
+  return answer.length ? answer.sort((x, y) => x - y) : [-1];
 }
+
+
+
+// 정민이 방법   
+function solution(arr, divisor) {
+  let answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % divisor == 0) answer.push(arr[i]);
+  }
+
+  if (answer.length == 0) answer.push(-1);
+
+  return answer.sort((x, y) => x - y);
+}
+
