9413ccdc2b3631124a960f9dfef17da985033abf
canmarkme <numnice@hanmail.net>
2021-10-20 09:35:15 +0900

    [new] 211020 TIL

--- 자료구조/비선형 자료구조/Graph/Graph_2.js
+++ 자료구조/비선형 자료구조/Graph/Graph_2.js
@@ -18,12 +18,20 @@ Graph.prototype.addEdge = function (v1, v2) {
 
 // + 추가
 // removeEdge(): 간선(edge) 삭제
+// v1: 출발
+// v2: 도착
 Graph.prototype.removeEdge = function (v1, v2) {
+
+  // edge에 연결되는 키 값(v1)이 있는지 확인
+  // list에 값(v2)이 있는지 indexOf로 확인
   if (this.edge[v1]) {
     let idx = this.edge[v1].indexOf(v2);
+
     if (idx != -1) {
-      this.edge[v1].splice(idx, 1);
+      this.edge[v1].splice(idx, 1);   // 인덱스로 받은 값과 배열 길이 1만큼 삭제합니다.
     }
+
+    // 키에 연결된 빈 배열까지 delete로 삭제합니다.
     if (this.edge[v1].length === 0) {
       delete this.edge[v1];
     }
@@ -32,9 +40,13 @@ Graph.prototype.removeEdge = function (v1, v2) {
 
 // removeVertex(): 정점(vertex) 삭제
 Graph.prototype.removeVertex = function (v) {
+  // edge[v]에 값 확인을 해서 return 처리하고 이후 코드를 빠져나옵니다.
   if (this.edge[v] === undefined) return;
-  let length = this.edge[v].length; // changed length
-  let connectedVertex = [...this.edge[v]]; // object copy
+
+  let length = this.edge[v].length;   // changed length
+  let connectedVertex = [...this.edge[v]];  // object copy
+
+  // 연결된 edge들을 모두 삭제해줍니다.
   for (let i = 0; i < length; i++) {
     this.removeEdge(v, connectedVertex[i]);
   }
@@ -63,7 +75,9 @@ console.log(graph.edge);
 graph.removeEdge("B", "F");
 graph.removeEdge("B", "E");
 console.log(graph.edge);
-graph.removeVertex("B");
+
+graph.removeVertex("B");    // 75줄과 결과 값이 동일
 console.log(graph.edge);
+
 graph.removeVertex("D");
 console.log(graph.edge);
