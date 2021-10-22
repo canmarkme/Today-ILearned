c4a25528ed3f9f3442b01bfef9ff47e3bd2da460
canmarkme <numnice@hanmail.net>
2021-10-22 22:23:14 +0900

    [update] add function()

--- PS/Think Outside the Box(TOB)/b4673/b4673.js
+++ PS/Think Outside the Box(TOB)/b4673/b4673.js
@@ -1,10 +1,11 @@
 // 입력 없음
 
-const N = 10000;
+solution();
 
+const N = 10000;
 const chkArr = Array(N + 1).fill(true);     // 0 ~ 10000까지 셀프넘버 배열을 생성하고 배열 값을 true로 초기화합니다.
 
-function solution(n) {
+function d(n) {
   let s = n;
 
   while (n) {
@@ -15,16 +16,15 @@ function solution(n) {
   return s + n;
 }
 
-for (let i = 1; i <= N; i++) {
-  const t = solution(i);
-  if (t < N) {
-    chkArr[t] = false;      // 셀프넘버가 아닌 수는 false
+function solution() {
+  for (let i = 1; i <= N; i++) {
+    const t = d(i);
+    if (t < N) {
+      chkArr[t] = false;      // 셀프넘버가 아닌 수는 false
+    }
+  }
+
+  for (let i = 1; i < N; i++) {
+    if (chkArr[i]) console.log(i);
   }
 }
-
-for (let i = 1; i < N; i++) {
-  if (chkArr[i]) console.log(i);
-}
-
-// Write your code
-console.log();
