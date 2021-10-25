868b69408f262a12a61c9c46bd94333620f639b6
canmarkme <numnice@hanmail.net>
2021-10-26 07:43:48 +0900

    [new] 211026 TIL

---
+++ PS_JS/BOJ/b1712/b1712.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+/**
+ * A: 고정비용
+ * B: 가변비용
+ * C: 노트북 가격
+ * M: 마진(Margin)
+ * P: 손익분기점(BREAK-EVEN POINT)
+ */
+
+console.log(solution());
+
+function solution() {
+  const A = input[0];
+  const B = input[1];
+  const C = input[2];
+
+  const M = C - B;
+  const P = Math.floor(A / M) + 1;
+  return P > 0 ? P : -1
+}
