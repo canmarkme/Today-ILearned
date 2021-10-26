3e8a91a4192459e665832c1ccf6e8488de525d94
canmarkme <numnice@hanmail.net>
2021-10-26 14:29:12 +0900

    [new] 211026 TIL

--- PS_JS/BOJ/b1712/b1712.js
+++ PS_JS/BOJ/b1712/b1712.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString().split('\n');
+let input = fs.readFileSync(filePath).toString().split(' ');
 
 /**
  * A: 고정비용
@@ -13,11 +13,12 @@ let input = fs.readFileSync(filePath).toString().split('\n');
 console.log(solution());
 
 function solution() {
-  const A = input[0];
-  const B = input[1];
-  const C = input[2];
+  const A = +input[0];
+  const B = +input[1];
+  const C = +input[2];
 
   const M = C - B;
   const P = Math.floor(A / M) + 1;
-  return P > 0 ? P : -1
+
+  return M > 0 ? P : -1;
 }
