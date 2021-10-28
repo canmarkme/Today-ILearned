a41059e396f3dd09daa0cdfd77b35685001597c5
canmarkme <numnice@hanmail.net>
2021-10-28 14:13:08 +0900

    [update] black zone matter

--- PS_JS/BOJ/b1193/b1193.js
+++ PS_JS/BOJ/b1193/b1193.js
@@ -3,13 +3,13 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let X = fs.readFileSync(filePath).toString().trim();
 let counter = 0;
 
-while (X > 0) {
-  counter++;
-  X = X - counter;
-}
+// while (X > 0) {
+//   counter++;
+//   X = X - counter;
+// }
 
-if (counter % 2 === 0) {
-  console.log(`${counter + X}/${1 + -X}`);
-} else {
-  console.log(`${1 + -X}/${counter + X}`);
-}
+// if (counter % 2 === 0) {
+//   console.log(`${counter + X}/${1 + -X}`);
+// } else {
+//   console.log(`${1 + -X}/${counter + X}`);
+// }
