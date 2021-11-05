dffbab0b37ec3d17d9e778c8eec3cbee116a82aa
canmarkme <numnice@hanmail.net>
2021-11-05 22:27:51 +0900

    [new] 211105 TIL

--- PS_JS/BOJ/b1193/b1193.js
+++ PS_JS/BOJ/b1193/b1193.js
@@ -1,15 +1,24 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let X = fs.readFileSync(filePath).toString().trim();
-let counter = 0;
+const input = fs.readFileSync(filePath).toString().trim();  // X번째
+const X = +input;
+let sum = 0;
+let index = 0;
+let line = 0;
 
-// while (X > 0) {
-//   counter++;
-//   X = X - counter;
-// }
+while (true) {
+  line++;
+  sum = (line * (line + 1)) / 2;
+  if (X <= sum) {
+    break;
+  }
+}
 
-// if (counter % 2 === 0) {
-//   console.log(`${counter + X}/${1 + -X}`);
-// } else {
-//   console.log(`${1 + -X}/${counter + X}`);
-// }
+
+// 2. X가 속한 라인에서 X의 인덱스
+// X(총길이) - (X-1)라인까지 값 
+index = X - ((line - 1) * line / 2);
+
+
+// 3. X가 속한 라인의 방향 찾기(짝수, 홀수)
+line % 2 === 0 ? console.log(`${index}/${line - index + 1}`) : console.log(`${line - index + 1}/${line}`);
--- PS_JS/zero-base/배열/나무그리기/나무그리기.js
+++ PS_JS/zero-base/배열/나무그리기/나무그리기.js
@@ -8,26 +8,43 @@ for (let i = 0; i < input.length; i++) {
   console.log(answer(input[i]));
 }
 
+// 강사님 방법
+// function answer(height) {
+//   let str = "\n";
+
+
+//   for (let i = 0; i < height; i++) {
+
+//     // 1. 공백 처리 ''
+//     for (let j = 0; j < height - i - 1; j++) {
+//       str += " ";
+//       //str += "🤍";
+//     }
+
+//     // 2. "*" 은 2n+1
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//       str += "*";
+//     }
+
+//     // 3. 개행 문자 추가
+//     str += "\n";
+//   }
+
+//   return str;
+// }
+
+
+// 이승연 수강생 방법
 function answer(height) {
   let str = "\n";
 
+  for (let i = 1; i < height + 1; i++) {
+    const whiteSpace = " ".repeat(height - i);
+    const numStars = "*".repeat(2 * i - 1);
+    str += `
+    ${whiteSpace}${numStars}${whiteSpace}`;
 
-  for (let i = 0; i < height; i++) {
-
-    // 1. 공백 처리 ''
-    for (let j = 0; j < height - i - 1; j++) {
-      str += " ";
-      //str += "🤍";
-    }
-
-    // 2. "*" 은 2n+1
-    for (let j = 0; j < 2 * i + 1; j++) {
-      str += "*";
-    }
-
-    // 3. 개행 문자 추가
-    str += "\n";
   }
-
   return str;
+
 }
