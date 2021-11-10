84f0bba6fd3051d9334d7c6ce606dce58e54904d
canmarkme <numnice@hanmail.net>
2021-11-10 22:12:30 +0900

    변수명 변경

--- PS_JS/Programmers/Level1/k번째수/k번째수.js
+++ PS_JS/Programmers/Level1/k번째수/k번째수.js
@@ -16,9 +16,9 @@ function solution(array, commands) {
   const answer = [];
 
   for (let i = 0; i < commands.length; i++) {
-    const elementCut = array.slice(commands[i][0] - 1, commands[i][1]);
-    elementCut.sort((x, y) => x - y);
-    answer.push(elementCut[commands[i][2] - 1]);
+    const newArr = array.slice(commands[i][0] - 1, commands[i][1]);
+    newArr.sort((x, y) => x - y);
+    answer.push(newArr[commands[i][2] - 1]);
   }
   return answer;
 }
