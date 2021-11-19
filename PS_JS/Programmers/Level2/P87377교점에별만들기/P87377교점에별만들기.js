d8cb0a1f9aa6761a80561927fdb0ca5c94920b3b
canmarkme <numnice@hanmail.net>
2021-11-19 14:44:40 +0900

    [new] 프로그래머스 교점에별만들기 solution

---
+++ PS_JS/Programmers/Level2/P87377교점에별만들기/P87377교점에별만들기.js
@@ -0,0 +1,74 @@
+let input = [
+  [2, -1, 4],
+  [-2, -1, 4],
+  [0, -1, 1],
+  [5, -8, -12],
+  [5, 8, 12]
+];
+
+// for (let i = 0; i < input.length; i++) {
+//   process.stdout.write(`#${i + 1} `);
+//   //console.log(solution(input));
+// }
+
+console.log(solution(input));
+
+
+function solution(line) {
+  const N = line.length;
+  const INF = Number.MAX_SAFE_INTEGER;
+  const crossPoints = [];
+  let minX = INF;
+  let minY = INF;
+  let maxX = -INF;
+  let maxY = -INF;
+
+  // 1. 문제 참고 사항 공식을 이용해 모든 정수 교차점과 좌표의 최대/최솟값을 구합니다.
+
+  for (let i = 0; i < N - 1; i++) {
+    for (let j = i + 1; j < N; j++) {
+      const [a, b, e] = line[i];
+      const [c, d, f] = line[j];
+      //console.log(`line[i] ${line[i]}`);
+      //console.log(`line[j] ${line[j]}`);
+
+      const mod = a * d - b * c;
+      if (!mod) continue; // 분모가 0인 경우 제외
+
+      const xNumerator = b * f - e * d;
+      const yNumerator = e * c - a * f;
+
+      if (xNumerator % mod || yNumerator % mod) continue;   // 실수 교차점
+
+
+      // 두 직선의 방정식이 교차되는 ( x , y ) 좌표가 나옵니다.
+      const x = xNumerator / mod;
+      const y = yNumerator / mod;
+
+      // 교점 추가
+      crossPoints.push([x, y]);
+
+      // x, y의 최솟값, 최댓값을 구합니다.
+      minX = Math.min(minX, x);
+      minY = Math.min(minY, y);
+      maxX = Math.max(maxX, x);
+      maxY = Math.max(maxY, y);
+    }
+  }
+
+
+  // 2. 너비와 높이를 계산 후, 별을 찍습니다.
+  // y의 높이 만큼, x의 길이를 '.'으로 채워진 2차 배열 paper를 만듭니다.
+  const paper = [...Array(maxY - minY + 1)].map(() => [...Array(maxX - minX + 1)].fill('.'));
+  //console.log(paper);
+  // 왜 최대y와 최소x에서 y와 x를 빼는가...
+
+  // [ [ -0, 4 ], [ -4, -4 ], [ 4, -4 ], [ 4, 1 ], [ -4, 1 ] ]
+  crossPoints.forEach(([x, y]) => {
+    paper[maxY - y][x - minX] = '*';
+
+  });
+
+
+  return paper.map(v => v.join(''));
+}
