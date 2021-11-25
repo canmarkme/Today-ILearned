999fd8d5ae71e9468f5c766ee8873cc6d19cf6ba
canmarkme <numnice@hanmail.net>
2021-11-25 12:45:36 +0900

    [new] 프로그래머스 압축 solution

---
+++ PS with js/Programmers/Level2/p17684압축/p17684압축.js
@@ -0,0 +1,40 @@
+function solution(msg) {
+  const answer = [];
+  const dict = ['']; // dict[0] 빈 값을 넣어줍니다.
+  let j = 0;
+
+  // 1. 사전 초기화
+  // A: 65, Z: 90
+  for (let i = 65; i < 91; i++) {
+    dict.push(String.fromCharCode(i));
+  }
+
+  console.log(dict);
+  for (let i = 0; i < msg.length; i = j) {
+    let w = msg[i];
+
+    for (j = i + 1; j < msg.length; j++) {
+      // 2. W+C 합친 문자열이 사전에 등록되어 있지 않았다면 사전에 등재합니다.
+      const c = msg[j];
+      if (!dict.includes(w + c)) {
+        dict.push(w + c);
+        break;
+      }
+
+      console.log(w); // K인데 A랑 합하면 KA라서 못들어가..
+      w += c;
+      console.log(w); // 둘이 합쳐 AB
+    }
+
+    // w 색인 번호를 출력합니다.
+    answer.push(dict.indexOf(w));
+  }
+  return answer;
+}
+// const input = ['ABABABABABABABAB'];
+const input = ['KAKAO'];
+
+for (let i = 0; i < input.length; i++) {
+  process.stdout.write(`#${i + 1} `);
+  console.log(solution(input[i]));
+}
