dd791e9f3f413f14077c5f13ab610036d9895d8e
canmarkme <numnice@hanmail.net>
2021-12-02 17:08:01 +0900

    [update] change BOJ form

--- PS with js/BOJ/b10/b10.js
+++ PS with js/BOJ/b10/b10.js
@@ -1,7 +1,10 @@
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+// let input = fs.readFileSync(filePath).toString().split('\n');
 const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString().split('\n');
 
+let input = fs.readFileSync('./input.txt').toString();
+input = input.split('\n');
 
 // Write your code
 console.log();
