620ff27d27538ab636cabc0671b3b9cec6cd8a31
canmarkme <numnice@hanmail.net>
2021-12-02 20:43:05 +0900

    [new] b6064 solution

---
+++ PS with js/BOJ/b6064/b6064.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().split('\n');
+
+input.shift(); // T개의 테스트 데이터
+
+for (const datas of input) {
+  const data = datas.split(' ').map(value => +value);
+  const M = data[0];
+  const N = data[1];
+  const x = data[2];
+  const y = data[3];
+
+  const max = M * N;
+  let k = -1; // k번째 해를 구하시오!
+  let index = 0;
+
+  while (true) {
+    if ((M * index + x) % N === y) {
+      k = M * index + x;
+      break;
+    }
+
+    if (M * index + x > max) break;
+    index++;
+  }
+  console.log(k);
+}
