c1fe3d3be831c9cb5b477ee15ac9bff317211ee9
canmarkme <numnice@hanmail.net>
2021-12-05 21:22:08 +0900

    [update] stack 괄호짝짓기 solution

--- PS with js/zero-base/스택/괄호짝찾기/괄호짝찾기.js
+++ PS with js/zero-base/스택/괄호짝찾기/괄호짝찾기.js
@@ -1,4 +1,3 @@
-
 // 괄호 연산은 스택으로 많이 해결합니다.
 // 괄호 매핑되는 것 끼리 짝을 이룹니다.
 // 시작 괄호와 끝나는 괄호와 매핑
@@ -12,18 +11,18 @@ if (!Array.prototype.peek) {
 
 if (!Array.prototype.isEmpty) {
   Array.prototype.isEmpty = function () {
-    return this.length == 0;
+    return this.length === 0;
   };
 }
 
 function answer(str) {
-  let result = [];
-  let stack = [];
+  const result = [];
+  const stack = [];
 
   for (let i = 0; i < str.length; i++) {
-    if (str[i] == "(") {
+    if (str[i] === '(') {
       stack.push(i);
-    } else if (str[i] == ")") {
+    } else if (str[i] === ')') {
       // "(" < ")" 인 경우
       if (stack.isEmpty()) {
         return [];
@@ -41,15 +40,7 @@ function answer(str) {
   return result;
 }
 
-
-
-let input = [
-  "(a+b)",
-  "(a*(b+c)+d)",
-  "(a*(b+c)+d+(e)",
-  "(a*(b+c)+d)+e)",
-  "(a*(b+c)+d)+(e*(f+g))",
-];
+const input = ['(a+b)', '(a*(b+c)+d)', '(a*(b+c)+d+(e)', '(a*(b+c)+d)+e)', '(a*(b+c)+d)+(e*(f+g))'];
 
 for (let i = 0; i < input.length; i++) {
   process.stdout.write(`#${i + 1} `);
